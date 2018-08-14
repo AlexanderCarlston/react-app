d2db2d45b29d7e7baf67c91365ec6bb197184591
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-08-14 10:59:53 -0600

    imported react leaflet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    location: {
+      lat: 51.505,
+      lng: -0.09,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
+    userMessage: {
+      name: '',
+      message: ''
+    },
+    showMessageForm: false,
+    sendingMessage: false,
+    sentMessage: false,
+    messages: []
+  }
+  
   render() {
     return (
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
+      <Map
+      className="map"
+      worldCopyJump={true}
+      center={position}
+      zoom={this.state.zoom}>
+      <TileLayer
+        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors and Chat location by Iconika from the Noun Project"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {
+        this.state.haveUsersLocation ? 
+        <Marker
+          position={position}
+          icon={myIcon}>
+        </Marker> : ''
+      }
+      {this.state.messages.map(message => (
+        <Marker
+          key={message._id}
+          position={[message.latitude, message.longitude]}
+          icon={messageIcon}>
+          <Popup>
+            <p><em>{message.name}:</em> {message.message}</p>
+            { message.otherMessages ? message.otherMessages.map(message => <p key={message._id}><em>{message.name}:</em> {message.message}</p>) : '' }
+          </Popup>
+        </Marker>
+      ))}
+    </Map>
     );
   }
 }
